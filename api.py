fd7bbddeecf127746329c2c18d2419cb528dfd6b
Abhinav Agarwal <119868907+Abhinavagarwa@users.noreply.github.com>
2024-09-28 09:14:49 +0530

    Update api.py

--- api.py
+++ api.py
@@ -9,9 +9,6 @@ CORS(app)
 
 model=tf.keras.models.load_model('./model_final.h5')
 print("model fetched")
-test_data = np.array([[75, 120.51, 40, 35, 50, 30, 0.7, 20, 3.93]])
-test_prediction = model.predict(test_data)
-print("Test Prediction (AQI):", test_prediction.flatten())
 
 feature_columns = ['PM2.5', 'PM10', 'NO', 'NO2', 'NOx', 'NH3', 'CO', 'SO2', 'Toluene']
 
